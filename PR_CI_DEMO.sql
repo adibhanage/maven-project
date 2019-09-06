f5079cbfd0952efcb46f554550275b52f794752d
adibhanage <adi.bhanage@esure.com>
2019-09-06 19:11:41 +0100

    V3

--- PR_CI_DEMO.sql
+++ PR_CI_DEMO.sql
@@ -3,7 +3,7 @@ CREATE OR REPLACE PROCEDURE pr_ci_demo
   vlast_name IN VARCHAR2 
 ) AS 
 BEGIN
-  -- UPDATE emp_demo SET salary = (salary * 0.002) + salary WHERE last_name = vlast_name;
+   UPDATE emp_demo SET salary = (salary * 0.002) + salary WHERE last_name = vlast_name;
   
   UPDATE emp_demo SET commission_pct = 0.03  WHERE last_name = vlast_name;
   
