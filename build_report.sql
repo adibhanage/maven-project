abc5c8da91bc951bee6c4e7a1d76c30241dd77dc
adibhanage <adi.bhanage@esure.com>
2019-09-16 14:55:25 +0100

    V12
    V12

--- build_report.sql
+++ build_report.sql
@@ -2,7 +2,7 @@ SET SERVEROUTPUT ON SIZE UNLIMITED
 SET WRAP OFF;
 SET FEEDBACK OFF;
 
-exec ut.run(ut_coverage_html_reporter());
+exec ut.run('ut3.ut_garrisons', ut_coverage_html_reporter());
 
 EXIT;
 
