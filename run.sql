082538f2813427a0eec2cb6faf628c67722c8f9a
adibhanage <adi.bhanage@esure.com>
2020-01-09 12:26:24 +0000

    Update run.sql

--- run.sql
+++ run.sql
@@ -1,1 +1,8 @@
-select 'Hello , today is = ' || to_char(sysdate, 'ddth Month yyyy') from dual;
+DECLARE
+v_greetings VARCHAR2(100);
+BEGIN
+select 'Hello , today is = ' || to_char(sysdate, 'ddth Month yyyy') INTO v_greetings from dual;
+
+DBMS_OUTPUT.PUT_LINE(v_greetings);
+END;
+/
